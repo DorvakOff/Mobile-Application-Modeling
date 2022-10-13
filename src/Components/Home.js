55e75739f53fd2692b8054bb15255b73cf4ecafd
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-10-13 12:20:48 +0200

    Add(updateHome) : Mise en page et ajour d'élément dans la page d'accueil

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,12 +7,33 @@ export default function App() {
                 <h1 className="text-center m-5">Mobile Modeling 2022</h1>
                 <div className="row">
                     <div className="col-12 m-5">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Bienvenue sur le site de Mobile Modeling 2022</h5>
-                                <p className="card-text">Ce site est en cours de construction.</p>
-                                <p className="card-text">Pour plus d'informations, veuillez contacter l'administrateur du site.</p>
-                                <a href="mailto:" className="btn btn-primary">Contacter l'administrateur</a>
+                        <div>
+                            <h2>Bienvenue sur le site de Mobile Modeling 2022</h2>
+                            <br/>
+                            <p>Réalisé par l'équipe jaune :</p>
+                            <div className="row">
+                                <div className="col-3">
+                                    <ul>
+                                        <li>Kévin Véronési</li>
+                                        <li>Noémie Tandol</li>
+                                        <li>Matéo Papaix</li>
+                                        <li>Fabien Bertato</li>
+                                        <li>Titouan Foras</li>
+                                    </ul>
+                                </div>
+                                <div className="col-3">
+                                    <ul>
+                                        <li>Noémie Rappeneau</li>
+                                        <li>Vincent Guillou</li>
+                                        <li>Camille Durand</li>
+                                        <li>Julien Devienne-Ousmer</li>
+                                        <li>Théo Villeneuve</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="mt-4">
+                                <a href="https://github.com/DorvakOff/Mobile-Application-Modeling" target="_blank"
+                                                               className="btn btn-primary">Contribuer</a>
                             </div>
                         </div>
                     </div>
