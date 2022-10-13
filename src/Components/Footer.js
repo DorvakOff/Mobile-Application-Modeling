b8c679a7de723d5bb14359a4a9d08491e09ec053
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-10-13 18:15:46 +0200

    Add(Style) : Ajout de l'adresse de l'iut et d'un espace pour le logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,9 @@ export default function App() {
                 <div className="container p-4">
                     <section className="mb-4">
                         © 2022 Copyright:
-                        Team jaune - LP APSIO 2022-2023
+                        Team jaune - LP APSIO 2022-2023 <br/>
+                        IUT de Blagnac <br/>
+                        1 Place Georges Brassens, 31700 Blagnac
                     </section>
                 </div>
             </footer>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,34 +8,42 @@ export default function App() {
                 <div className="row">
                     <div className="col-12 p-5">
                         <div>
-                            <h2>Bienvenue sur le site de Mobile Modeling 2022</h2>
+                            <h2 className="text-center">Bienvenue sur le site de Mobile Modeling 2022</h2>
+                            <p className="text-center"> Site du cours de modélisation des applications mobiles de la LP
+                                APSIO 2011 -2022. </p>
                             <br/>
                             <p>Réalisé par l'équipe jaune :</p>
                             <div className="row">
-                                <div className="col-3">
-                                    <ul>
-                                        <li>Kévin Véronési</li>
-                                        <li>Noémie Tandol</li>
-                                        <li>Matéo Papaix</li>
-                                        <li>Fabien Bertato</li>
-                                        <li>Titouan Foras</li>
-                                        <li>Anthony Delbouis</li>
-                                    </ul>
-                                </div>
-                                <div className="col-3">
-                                    <ul>
-                                        <li>Noémie Rappeneau</li>
-                                        <li>Vincent Guillou</li>
-                                        <li>Camille Durand</li>
-                                        <li>Julien Devienne-Ousmer</li>
-                                        <li>Théo Villeneuve</li>
-                                    </ul>
+                                <div className="row">
+                                    <div className="col-3">
+                                        <ul>
+                                            <li>Kévin Véronési</li>
+                                            <li>Noémie Tandol</li>
+                                            <li>Matéo Papaix</li>
+                                            <li>Fabien Bertato</li>
+                                            <li>Titouan Foras</li>
+                                            <li>Anthony Delbouis</li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-4">
+                                        <ul>
+                                            <li>Noémie Rappeneau</li>
+                                            <li>Vincent Guillou</li>
+                                            <li>Camille Durand</li>
+                                            <li>Julien Devienne-Ousmer</li>
+                                            <li>Théo Villeneuve</li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-4">
+                                    {/*    charger une image de test */}
+                                        <img src="https://upload.wikimedia.org/wikipedia/fr/8/8a/Logo_IUT_Blagnac.png" alt={"logo IUT Blagnac"} className="img-fluid"/>
+                                    </div>
                                 </div>
                             </div>
                             <div className="mt-4">
                                 <a href="https://github.com/DorvakOff/Mobile-Application-Modeling" target="_blank"
                                    rel="noreferrer"
-                                    className="btn btn-primary">Contribuer</a>
+                                   className="btn btn-primary">Contribuer</a>
                             </div>
                         </div>
                     </div>
